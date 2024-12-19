0bf31aa4c759b393c4e4dc38f66cdb95d2135996
Salah Saleh <salah.saleh.ali@gmail.com>
2024-12-19 10:34:01 +0100

    fix issue with text wrapping

--- app/helpers/icon_helper.rb
+++ app/helpers/icon_helper.rb
@@ -29,6 +29,14 @@ module IconHelper
       render_cog_icon(options)
     when :bars_3
       render_bars_3_icon(options)
+    when :heart
+      render_heart_icon(options)
+    when :share
+      render_share_icon(options)
+    when :clipboard
+      render_clipboard_icon(options)
+    when :arrow_down
+      render_arrow_down_icon(options)
     else
       raise ArgumentError, "Unknown icon: #{name}"
     end
@@ -120,6 +128,30 @@ module IconHelper
   def render_bars_3_icon(options)
     tag.svg xmlns: "http://www.w3.org/2000/svg", fill: "none", viewBox: "0 0 24 24", stroke_width: "1.5", stroke: "currentColor", **options do
       tag.path stroke_linecap: "round", stroke_linejoin: "round", d: "M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+    end
+  end
+
+  def render_heart_icon(options)
+    tag.svg xmlns: "http://www.w3.org/2000/svg", fill: "none", viewBox: "0 0 24 24", stroke_width: "1.5", stroke: "currentColor", **options do
+      tag.path stroke_linecap: "round", stroke_linejoin: "round", d: "M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+    end
+  end
+
+  def render_share_icon(options)
+    tag.svg xmlns: "http://www.w3.org/2000/svg", fill: "none", viewBox: "0 0 24 24", stroke_width: "1.5", stroke: "currentColor", **options do
+      tag.path stroke_linecap: "round", stroke_linejoin: "round", d: "M7.217 10.907a2.25 2.25 0 100 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186l9.566-5.314m-9.566 7.5l9.566 5.314m0 0a2.25 2.25 0 103.935 2.186 2.25 2.25 0 00-3.935-2.186zm0-12.814a2.25 2.25 0 103.933-2.185 2.25 2.25 0 00-3.933 2.185z"
+    end
+  end
+
+  def render_clipboard_icon(options)
+    tag.svg xmlns: "http://www.w3.org/2000/svg", fill: "none", viewBox: "0 0 24 24", stroke_width: "1.5", stroke: "currentColor", **options do
+      tag.path stroke_linecap: "round", stroke_linejoin: "round", d: "M15.666 3.888A2.25 2.25 0 0013.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 01-.75.75H9a.75.75 0 01-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 01-2.25 2.25H6.75A2.25 2.25 0 014.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 011.927-.184"
+    end
+  end
+
+  def render_arrow_down_icon(options)
+    tag.svg xmlns: "http://www.w3.org/2000/svg", fill: "none", viewBox: "0 0 24 24", stroke_width: "1.5", stroke: "currentColor", **options do
+      tag.path stroke_linecap: "round", stroke_linejoin: "round", d: "M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3"
     end
   end
 end
