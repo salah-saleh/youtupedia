d9936bb86ae2cfae92445c0992208a666dd356ce
Salah Saleh <salah.saleh.ali@gmail.com>
2025-01-03 18:23:29 +0100

    move login/theme to sidebar

--- app/helpers/icon_helper.rb
+++ app/helpers/icon_helper.rb
@@ -37,6 +37,10 @@ module IconHelper
       render_clipboard_icon(options)
     when :arrow_down
       render_arrow_down_icon(options)
+    when :user
+      render_user_icon(options)
+    when :arrow_right
+      render_arrow_right_icon(options)
     else
       raise ArgumentError, "Unknown icon: #{name}"
     end
@@ -152,6 +156,18 @@ module IconHelper
   def render_arrow_down_icon(options)
     tag.svg xmlns: "http://www.w3.org/2000/svg", fill: "none", viewBox: "0 0 24 24", stroke_width: "1.5", stroke: "currentColor", **options do
       tag.path stroke_linecap: "round", stroke_linejoin: "round", d: "M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3"
+    end
+  end
+
+  def render_user_icon(options)
+    tag.svg xmlns: "http://www.w3.org/2000/svg", fill: "none", viewBox: "0 0 24 24", stroke_width: "1.5", stroke: "currentColor", **options do
+      tag.path stroke_linecap: "round", stroke_linejoin: "round", d: "M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+    end
+  end
+
+  def render_arrow_right_icon(options)
+    tag.svg xmlns: "http://www.w3.org/2000/svg", fill: "none", viewBox: "0 0 24 24", stroke_width: "1.5", stroke: "currentColor", **options do
+      tag.path stroke_linecap: "round", stroke_linejoin: "round", d: "M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9"
     end
   end
 end
