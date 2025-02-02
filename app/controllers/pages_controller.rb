0cad864bd1661ad6fc791d606fdf1e9337b8c746
Salah Saleh <salah.saleh.ali@gmail.com>
2025-02-02 22:24:54 +0100

    init discover page with contents

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
-class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:about, :contact, :discover]
+class PagesController < PublicController
+  include Paginatable
 
   def about
   end
@@ -8,5 +8,37 @@ class PagesController < ApplicationController
   end
 
   def discover
+    # Get video IDs based on search or user's collection
+    video_ids = if params[:q].present?
+      Search::VideoSearchService.search_video_ids(params[:q], "master")
+    else
+      UserServices::UserDataService.user_items("master", :summaries)
+    end
+
+    return @summaries = [] if video_ids.empty?
+
+    # Apply pagination to video IDs
+    paginated_video_ids = paginate(video_ids)
+
+    # Fetch metadata for paginated IDs
+    metadata_results = Youtube::YoutubeVideoMetadataService.fetch_metadata_batch(paginated_video_ids)
+
+    # Format results
+    @summaries = metadata_results.map do |video_id, metadata|
+      next unless metadata[:success]
+
+      published_at = metadata[:metadata][:published_at]
+      published_at = published_at.is_a?(String) ? DateTime.parse(published_at) : published_at
+
+      {
+        video_id: video_id,
+        title: metadata[:metadata][:title],
+        channel: metadata[:metadata][:channel_title],
+        published_at: published_at,
+        thumbnail: metadata[:metadata][:thumbnails][:high]
+      }
+    end.compact
+
+    respond_with_pagination(turbo_frame_id: "discover_content")
   end
 end 
